48fb9f345643737c9783116df1fb983103183e2b
Chick <chickenduck562@gmail.com>
2021-06-02 09:41:09 -0700

    updated function docData

--- config/connectToDataBase.js
+++ config/connectToDataBase.js
@@ -1,44 +1,52 @@
 import config from 'config';
-import firebase from 'firebase';
- var firebaseConfig = {
-    apiKey: "AIzaSyBylRAbrNUWQRkppjgE6vwr11Xaue70vls",
-    authDomain: "legends-bot-6a482.firebaseapp.com",
-    projectId: "legends-bot-6a482",
-    storageBucket: "legends-bot-6a482.appspot.com",
-    messagingSenderId: "801454364629",
-    appId: "1:801454364629:web:85cd14c5bd2d83869acf5c",
-    measurementId: "G-G107FR5SGK"
-    };
+import firebase from "firebase/app";
+import "firebase/firestore";
+import 'firebase/analytics';
 
-    if (!firebase.apps.length) {
-        firebase.initializeApp(firebaseConfig);
-    } else {
-        firebase.app();
-    }
+var firebaseConfig = {
+  apiKey: "AIzaSyBylRAbrNUWQRkppjgE6vwr11Xaue70vls",
+  authDomain: "legends-bot-6a482.firebaseapp.com",
+  projectId: "legends-bot-6a482",
+  storageBucket: "legends-bot-6a482.appspot.com",
+  messagingSenderId: "801454364629",
+  appId: "1:801454364629:web:85cd14c5bd2d83869acf5c",
+  measurementId: "G-G107FR5SGK"
+};
 
-    firebase.analytics();
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+} else {
+  firebase.app();
+}
+firebase.analytics();
 
 async function getDocData(data) {
   const doc = await data.get();
   return doc.data();
 }
 
+var prefix = null;
+var guildID = null;
+var guildName = null;
+
 function docData(doc) {
-    getDocData(doc).then((result) => {
-        
-    });
+  getDocData(doc).then((result) => {
+    prefix = result["prefix"];
+    guildID = result['guildID'];
+    guildName = result['guildName'];
+  });
 }
 
 export default function () {
-    const serverName = 'LEGENDS'; //later will update to make it read server name
-    const db = firebase.firestore();
-    const guildData = db.collection('guild-data');
-    const guild = guildData.doc(serverName);
-    const serverData = guild.collection(serverName + '-GUILD-DATA');
-    const prefixData = serverData.doc('prefix');
+  const serverName = "LEGENDS"; //later will update to make it read server name
+  const db = firebase.firestore();
+  const guildData = db.collection("guild-data");
+  const guild = guildData.doc(serverName);
+  const serverData = guild.collection(serverName + "-GUILD-DATA");
+  const prefixData = serverData.doc("prefix");
+  prefixData.set({
+    prefix: "kak"
+  });
 
-    const prefix = 'kak'; // change later
-    prefixData.set({
-        'prefix': prefix
-    })
 }
+
