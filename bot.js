2d78221a52cba2460db1eca060b031e58d55570a
Itai Hammer <61294759+ItaiHammer@users.noreply.github.com>
2021-06-02 12:16:51 -0700

    Made bot use routes/guilds vars

--- bot.js
+++ bot.js
@@ -6,36 +6,39 @@ dotenv.config();
 import { Ping, Help } from './commands.js';
 import { memeChannelVoting } from './auto.js';
 
+//routes
+import * as server from './routes/guilds.js';
+
 const client = new Discord.Client();
 
 client.on('ready', () => console.log(`Bot connected to ${client.user.tag}`));
 
 client.on('guildCreate', (guild) => {
-    console.log(`Guild was created at: ${guild.name}`);
+  console.log(`Guild was created at: ${guild.name}`);
 });
 
 client.on('guildDelete', (guild) => {
-    console.log(`Guild was deleted from: ${guild.name}`);
+  console.log(`Guild was deleted from: ${guild.name}`);
 });
 
 client.on('message', (msg) => {
-    //temporary prefix (we'll change this later)
-    const prefix = '!';
+  //temporary prefix (we'll change this later)
+  const prefix = server.getItem(prefix);
 
-    memeChannelVoting(msg);
+  memeChannelVoting(msg);
 
-    if (!msg.content.substr(0, prefix.length) === prefix) return;
-    msg.content = msg.content.substr(prefix.length);
-    const query = msg.content.split(' ');
+  if (!msg.content.substr(0, prefix.length) === prefix) return;
+  msg.content = msg.content.substr(prefix.length);
+  const query = msg.content.split(' ');
 
-    switch (query[0]) {
-        case 'help':
-            new Help().run(msg);
-            break;
-        case 'ping':
-            new Ping().run(msg);
-            break;
-    }
+  switch (query[0]) {
+    case 'help':
+      new Help().run(msg);
+      break;
+    case 'ping':
+      new Ping().run(msg);
+      break;
+  }
 });
 
 client.login(process.env.TOKEN);
